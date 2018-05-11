9f1870b578c71076cd887d7ee5f9d785898d380f
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-05-11 09:06:58 +0400

    duplicate handling not working

--- website/static/js/custom.js
+++ website/static/js/custom.js
@@ -1,22 +1,21 @@
 function exists(element) {
 
-	element.each(function(){
-		var li = $(this).find('li');
-
-		console.log(li.text());
-		console.log($( "#indicators option:selected" ).text());
-
-		// if(li.text() == $( "#indicators option:selected" ).text()) {
-		// 	return true;
-		// }
-	});
-	return false;
+		element.find('li').each(function(){
+			// console.log($(this).text());
+			// console.log($( "#indicators option:selected" ).text());
+			if($(this).text() == $( "#indicators option:selected" ).text()) {
+				console.log("Giorgi");
+				return true;
+			}
+		});
+		return false;
 }
 
 $( "#add" ).click(function() {
-
-	if(!exists($("#formula"))) {
+	exists($("#formula"));
+	// if(!exists($("#formula"))) {
 		$("#formula").append('<li class="list-group-item">'+ $( "#indicators option:selected" ).text() +'</li>');
-	}
+	// }
+	console.log(exists($("#formula")));
 
 });
