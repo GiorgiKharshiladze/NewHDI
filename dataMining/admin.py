588b551ce4a119c62a0ea7a4458fc8966cb786c3
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-05-25 00:21:50 +0400

    model added into admin

--- dataMining/admin.py
+++ dataMining/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from dataMining.models import Indicator
 
 # Register your models here.
+
+admin.register(Indicator)(admin.ModelAdmin)
+
+# admin.register(one, two, three)(admin.ModelAdmin)
