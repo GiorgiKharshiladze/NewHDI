06bb57dde6fae85a80281145b2338e6efe356a76
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-05-26 10:53:52 +0400

    comment cleanup

--- dataMining/views.py
+++ dataMining/views.py
@@ -66,30 +66,3 @@ def getLocal(request, my_id):
 	serializer = IndicatorSerializer(indicator)
 
 	return Response(serializer.data)
-
-
-
-# ============================================
-# REMOVED: Fill in the indicators to local db
-# ============================================
-# def fill_indicators_table(request):
-
-# 	test_url = "http://api.worldbank.org/v2/indicators?format=json"
-# 	per_page = requests.get(url=test_url).json()[0]['total']
-
-# 	my_url = test_url + "&per_page="+str(per_page)
-# 	r = requests.get(url=my_url)
-# 	counter = 0
-# 	for i in r.json()[1]:
-# 		# print(i["id"]) # indicator_id
-# 	    # print(i["source"]["value"]) # title
-# 	    # print(i["sourceNote"]) # description
-
-# 		new_indicator = Indicator(indicator_id=i["id"], title=i["source"]["value"], description=i["sourceNote"])
-
-# 		counter += 1
-# 		print(str(counter)+ ": " + str(new_indicator))
-
-# 		new_indicator.save()
-
-# 	return None
