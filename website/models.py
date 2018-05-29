8fdc4b3ab02a70952b5cca575f68fb72289514c9
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-05-30 00:32:31 +0400

    create page migration

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+
+	def __str__(self):
+		return str(self.title)
+
+	class Meta:
+		db_table = 'pages'
+
+	id = models.AutoField(primary_key=True)
+	title = models.CharField(max_length=255)
+	content = models.TextField(null=True, blank = True)
+	created_at = models.DateTimeField(auto_now_add=True, null=True)
+	updated_at = models.DateTimeField(auto_now=True, null=True)
