15aa1953e003ca9bf6ae23d115fefe4b0d056efc
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-05-30 00:12:03 +0400

    Better workflow inside the templates

--- website/urls.py
+++ website/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls import url
 urlpatterns = [
 	# Main Routes
 	url(r'^$', views.index, name='index'),
-	url(r'^customHDI/$', views.customHDI, name='customHDI'),
-	url(r'^test/$', views.test, name='test'),
 
-	# Routes for testing
+	# Main Temporary Routes
+	url(r'^test/$', views.test, name='test'),
+	url(r'^customHDI/$', views.customHDI, name='customHDI'),
+
+	# Testing Routes
 	url(r'^sample/$', views.sample, name='sample'),
 	url(r'^example/$', views.example, name='example'),
 ]
