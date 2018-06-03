82c46ebec745468aaf0bce7cdb20d56d4481f145
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-06-03 13:06:58 +0400

    on change of indicator value

--- website/static/assets/js/scripts.js
+++ website/static/assets/js/scripts.js
@@ -7,3 +7,20 @@
   PLACE HERE YOUR OWN JAVASCRIPT CODE IF NEEDED
   WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR JAVASCRIPT CODE PLEASE CONSIDER WRITING YOUR SCRIPT HERE.  */
 })(window);
+
+var amount = $("#chooseAmount").val();
+
+if (amount == null) {
+		$("#createButton").attr("disabled", "");
+}
+
+$("#chooseAmount").on("change", function() {
+	if ($("#chooseAmount").val() != null) {
+		$("#createButton").removeAttr("disabled");
+	}
+});
+
+function createFormula() {
+
+	document.location = window.location.href + "?amount=" + amount;
+}
