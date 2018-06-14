af86b8f0f85ce3ba0e3bcdc733970149f5d656e2
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-06-14 16:41:31 +0400

    Interaction model created

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Interaction(models.Model):
+
+	def __str__(self): # Value that we see in DJANGO ADMIN
+		return str(self.data) + " - " + str(self.category)
+
+	class Meta:
+		db_table = 'interactions'
+
+	id = models.AutoField(primary_key=True)
+	data = models.TextField(null=True, blank = True)
+	category = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True, null=True)
+	updated_at = models.DateTimeField(auto_now=True, null=True)
